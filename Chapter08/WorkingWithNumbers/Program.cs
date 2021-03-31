8b43f7b3858f91d63f0c458e0be25fcea54ed6d3
palw <tolpawsta@gmail.com>
2021-04-01 02:00:45 +0300

    Working with complex numbers

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -10,10 +10,17 @@ namespace WorkingWithNumbers
         static void Main(string[] args)
         {
             var largest = ulong.MaxValue;
-            WriteLine($"{largest, 40:N0}");
+            WriteLine($"{largest, 60:N0}");
             var atomsInTheUniverse =
-                BigInteger.Parse("123456789012345678901234567890");
-            WriteLine($"{atomsInTheUniverse, 40:N0}");
+                BigInteger
+                    .Parse("12345678901234567890123456789012345678901234567890");
+            WriteLine($"{atomsInTheUniverse, 60:N0}");
+
+            // Working With Numbers with complex number
+            var c1 = new Complex(4, 2);
+            var c2 = new Complex(3, 7);
+            var c3 = c1 + c2;
+            WriteLine($"{c1} added to {c2} is {c3}");
         }
     }
 }
