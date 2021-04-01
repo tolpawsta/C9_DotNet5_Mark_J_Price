81aca87eff2b156fe8ad692e1c01ec262b4daeae
palw <tolpawsta@gmail.com>
2021-04-01 23:45:54 +0300

    Create WorkingWithNetworkRsources project

---
+++ Chapter08/WorkingWithNetworkResources/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithNetworkResources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
