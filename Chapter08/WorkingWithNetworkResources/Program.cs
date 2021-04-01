7ac86604071ce5d119c8be68e96a466831759e2b
palw <tolpawsta@gmail.com>
2021-04-01 23:58:18 +0300

    Add code to class Program

--- Chapter08/WorkingWithNetworkResources/Program.cs
+++ Chapter08/WorkingWithNetworkResources/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+
+using static System.Console;
 
 namespace WorkingWithNetworkResources
 {
@@ -6,7 +9,19 @@ namespace WorkingWithNetworkResources
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Write("Enter a valid web address: ");
+            string url = ReadLine();
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                url = "https//:world/episerver.com/cms/&q=pagetype";
+            }
+            var uri = new URI(url);
+            WriteLine($"URL: {url}");
+            WriteLine($"Scheme: {uri.Scheme}");
+            WriteLine($"Port: {uri.Port}");
+            WriteLine($"Host: {uri.Host}");
+            WriteLine($"Path: {uri.AbsolutePath}");
+            WriteLine($"Query: {uri.Query}");
         }
     }
 }
